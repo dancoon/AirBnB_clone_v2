46e8aa254bd4f88a3a0cebf8af1ad4c672e6039f
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-07-07 10:21:35 +0300

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
 
-# Install Nginx if not already installed
-if ! which nginx > /dev/null 2>&1; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-
-# Create necessary folders if they don't exist
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create a fake HTML file
-echo "<html><head><title>Test Page</title></head><body><h1>This is a test page</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -hR ubuntu:ubuntu /data/
-
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-# Restart Nginx
-sudo service nginx restart
+sudo service nginx start
